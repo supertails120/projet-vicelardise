805988069772a689ad2478b8cabba18f13b71685
supertails120 <106885031+supertails120@users.noreply.github.com>
2026-03-18 13:57:03 +0100

    console log du panneau

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const ace = document.getElementById("ace");
+const negropal = document.getElementById("negropal");
+
+ace.addEventListener('click',()=>{
+    console.log(ace);
+});
+negropal.addEventListener('click',()=>{
+    console.log(negropal);
+});
